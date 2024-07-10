c932a7e9a7b99f41db5c67b52039bff166cb5bde
Kyaw1511 <kyawminthein151193@gmail.com>
2024-07-10 22:44:02 +0630

    array part1

---
+++ arraypart1.js
@@ -0,0 +1,15 @@
+let people = [
+    "Kyaw Kyaw",
+    28,
+    "Kamayut Township",
+    [
+        "C", "Java", "php", "vue"
+    ]
+];
+document.getElementById("test1").innerHTML = `${people} <br>`;
+
+let arr = [ 22, 33,44, 55, 66, 77, 11, 88, 0, 99];
+for (let i = 0; i , arr.length; i++) {
+    console.log("Index => " + i + " => " + arr[i]);
+}
+console.log(people);
