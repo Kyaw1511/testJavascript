112839285a78c8c9d59e332855ebd1f6d3ec91e4
Kyaw1511 <kyawminthein151193@gmail.com>
2024-07-12 22:22:31 +0630

    find prime number.

---
+++ primeNum.js
@@ -0,0 +1,32 @@
+let inputd = document.querySelector("#sub-id");
+let cleard = document.querySelector("#id-clear");
+let result = document.getElementById("dom-msg");
+
+inputd.onclick = () => {
+    let num = Number(document.querySelector("#input-id").value);
+    if (num < 2) {
+        result.innerHTML = "Hello";
+        // console.log("false");
+        return false;
+    }
+    for (
+        let i = 2;
+        i <= Math.sqrt(num);
+        i++
+    ) {
+        if (num % i === 0) {
+            result.innerHTML = "This is not prime number."
+            // console.log("true");
+            return false;
+        }
+    }
+    result.innerHTML = "This is prime number.";
+    // console.log("hello world");
+    return true;
+};
+cleard.onclick = () => {
+    let num2 = document.querySelector("#input-id");
+    num2.value = "";
+    result.innerHTML = "";
+    // console.log("test")
+}
