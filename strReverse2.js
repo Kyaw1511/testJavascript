b45c06aaabadb502059058c5a835fd295da7a28a
Kyaw1511 <kyawminthein151193@gmail.com>
2024-07-14 22:31:55 +0630

    string reverse 1.1

--- strReverse2.js
+++ strReverse2.js
@@ -1,21 +1,23 @@
-// String reverse;
+// string reverse method 2;
 const inputS = document.getElementById("sub-str");
 const clearS = document.getElementById("str-clear");
-const result = document.getElementById("dom-msg");
-const origin = document.getElementById("org-str");
+const origin = document.getElementById("dom-msg");
+const result = document.getElementById("org-str");
 
 inputS.onclick = () => {
     let str = document.getElementById("input-str").value;
     function reverseStr(str) {
-        let newArray = str.split(""); //str to array;
-
-        return newArray.reverse().join(""); // join method is join for array to string.
+        let newStr = "";
+        for(let i = str.length - 1; i >= 0; i--) {
+            newStr += str[i];
+        }
+        return newStr;
     }
     origin.innerHTML = str;
     result.innerHTML = reverseStr(str);
-    // console.log(str);
     // console.log(reverseStr(str));
 }
+
 clearS.onclick = () => {
     let str = document.getElementById("input-str");
     str.value = "";
