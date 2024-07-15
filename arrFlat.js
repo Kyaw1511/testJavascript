fd0ab5a778fff481d3817f2cd0bd4810521a6087
Kyaw1511 <kyawminthein151193@gmail.com>
2024-07-15 21:54:04 +0630

    test for 3 array

--- arrFlat.js
+++ arrFlat.js
@@ -4,13 +4,29 @@ const array2 = document.getElementById("array2");
 const array3 = document.getElementById("array3");
 const array4 = document.getElementById("array4");
 
-let arr = [[10, 20, 60], [30, 40, 50], [33, 44, 55]];
+let arr = [[11, 22, 33], [44, 55, 66]];
+let arry = [[10, 20, 60], [30, 40, 50], [33, 44, 55]];
 for(let i = 0; i < arr.length; i++) {
     for(let x = 0; x < arr[i].length; x++) {
         array2.innerHTML += `Array ${i} of ${x} ==> ${arr[i][x]} <br>`;
     }
     array.innerHTML += `${arr[i]} <br>`;
     console.log(arr[i]);
+}
+for(
+    let z = 0; z < arry.length; z++
+) {
+    for(
+        let b = 0; b < arry[z].length; b++
+    ) {
+        for(
+            let c = 0; c < arry[b].length; c++
+        ) {
+            array4.innerHTML = `${arry} <br>`;
+            console.log(arry[c])
+        }
+    } 
+    
 }
 let newArr = arr.flat();
 array3.innerText = newArr;
