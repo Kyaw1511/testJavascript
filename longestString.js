b5b0b843a970367616b3bcebbe6aa031240023fa
Kyaw1511 <kyawminthein151193@gmail.com>
2024-07-17 21:34:16 +0630

    sorting num

--- longestString.js
+++ longestString.js
@@ -8,6 +8,7 @@ let arr = [
     "Python",
     "Java"
 ]; 
+let num = [10, 20, 33, 44, 11, 5, 7, 90, 1011];
 let longestStr = "";
 let secStr = "";
 let thirdStr = "";
@@ -28,4 +29,22 @@ document.getElementById("longestString").innerHTML = longestStr;
 console.log(longestStr);
 
 console.log(arr.sort());
-document.getElementById("sorting").innerHTML = `<li>${arr.sort()}</li>`;
+document.getElementById("days").innerHTML += arr.sort() + "<br>";
+document.getElementById("sorting").innerHTML += arr.sort().join("<br>");
+
+// sorting reverse for string
+document.getElementById("reverse").innerHTML += arr.sort().reverse().join("<br>");
+
+// test array for num
+document.getElementById("num1").innerHTML += num.sort();
+document.getElementById("num2").innerHTML += `<li>${num.sort()}</li>`;
+document.getElementById("num3").innerHTML += num.sort() + "<br>";
+document.getElementById("num4").innerHTML += num.sort().join("<br>");
+
+// sorting increasing number;
+document.getElementById("num5").innerHTML += num.sort((a,b) => a-b).join("<br>");
+// sorting decreasing number;
+document.getElementById("num6").innerHTML += num.sort((a,b) => b-a).join("<br>");
+
+// find array no 2;
+document.getElementById("numRoom2").innerHTML += num.sort((a,b) => b-a)[2];
