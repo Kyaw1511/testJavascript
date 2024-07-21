9a9ec0e10e9dfc0c29c46ce53e7a8487b91b3d5b
Kyaw1511 <kyawminthein151193@gmail.com>
2024-07-21 22:05:46 +0630

    fetch api using for

--- fetchAPI.js
+++ fetchAPI.js
@@ -1,4 +1,6 @@
 // fetch api
+let apiTest = document.getElementById("apiTest");
+let text = "";
 
 fetch("people.json")
 .then(res => res.json())
@@ -8,10 +10,17 @@ fetch("people.json")
 .finally(() => console.log("Fetch data completed."))
 
 // using jsonplaceholder.org
-fetch("https://jsonplaceholder.org/posts")
+fetch("https://jsonplaceholder.org/users")
 // fetch("https://jsonplaceholder.typicode.com/posts")
 .then(ans => ans.json())
-.then(data => console.log(data[0].userId))
+// .then(data => console.log(data[0]))
+// .then(data => apiTest.innerHTML += data[0].category)
+.then(data => {
+    for(let i = 0; i < data.length; i++) {
+        text +=i + " " + data[i].firstname + "<br>";
+    }
+    apiTest.innerHTML = text;   
+})
 .catch(error => console.log(error))
 .finally(() => console.log("Fetch data json placeholder is completed."))
 
