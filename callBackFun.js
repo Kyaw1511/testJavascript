3c6c8653286a85f962d64f8ef9e8d8d17a3b67bc
Kyaw1511 <kyawminthein151193@gmail.com>
2024-07-21 19:28:22 +0630

    asyn function, await keyword and newpromise

--- callBackFun.js
+++ callBackFun.js
@@ -37,3 +37,51 @@ function showRes(result) {
     console.log("This is .......", result);
 }
 addNumber(showRes);
+
+// promise function;
+function sumNum() {
+    let add = 0;
+    for(let i = 1; i<= 5000; i++) {
+        add += i;
+    }
+    return add;
+};
+function sumLater() {
+    return new Promise((resolve, reject) => {
+        let ans = sumNum();
+        if(ans) resolve(ans);
+        else reject();
+    })
+};
+sumLater().then(ans => console.log(ans)).catch(error => console.log(error));
+
+// promise function => error
+function sunNum2() {
+    const add2 = 0;
+    for(let i = 1; i <= 5000; i++) {
+        add2 += i;
+    }
+    return add2;
+};
+function sunLater2() {
+    return new Promise((resolve, reject) => {
+        let ans2 = sunNum2();
+        if(ans2) resolve(ans2);
+        else reject();
+    })
+};
+sunLater2().then(ans2 => console.log(ans2)).catch(error => console.log(error));
+
+// asyn function and using await keyword;
+function sunNum3() {
+    let add3 = 0;
+    for(let i = 1; i <= 5000; i++) {
+        add3 += i;
+    }
+    return add3;
+};
+async function sunLater3() {
+    let ans3 = await sunNum3();
+    console.log("async"+ans3);
+};
+sunLater3();
