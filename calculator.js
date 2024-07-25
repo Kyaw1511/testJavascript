c6c87a3da2b23957f3998317fa70764c50806c5a
Kyaw1511 <kyawminthein151193@gmail.com>
2024-07-25 21:36:11 +0630

    calculator v1.3

--- calculator.js
+++ calculator.js
@@ -1,1 +1,35 @@
 // calculator project
+const display = document.querySelector("#display");
+const buttons = Array.from(document.querySelectorAll(".button"));
+// buttons = document.querySelectorAll(".button");
+
+buttons.map((button) => {
+    button.addEventListener("click", calc);  
+});
+function calc(e) {
+    let value = e.target.innerText;
+    switch(value) {
+        case 'AC'   : display.innerText = "";
+                    break;
+        case '←'    : display.innerText = display.innerText.slice(0, -1);
+                    break;
+        case '='    :   try {
+                            if(display.innerText === '') {
+                                display.innerText = "";
+                                break;
+                            }
+                            else {
+                                display.innerText = eval(display.innerText);
+                                break;
+                            }
+                        }   catch(e) {
+                                display.innerText = "ERROR";
+                                break;
+                            }
+        default :   display.innerText += value;
+    }
+    console.log(e.target.innerText);
+}
+
+console.log(buttons);
+console.log(typeof(buttons));
