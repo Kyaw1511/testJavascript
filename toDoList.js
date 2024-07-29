f32a8f49d3929e1d8be669342245255a63066255
Kyaw1511 <kyawminthein151193@gmail.com>
2024-07-29 22:05:15 +0630

    to do list

--- toDoList.js
+++ toDoList.js
@@ -1,1 +1,19 @@
 // To Do Lists 
+const form = document.querySelector(".input-box");
+const tasks = document.querySelector(".tasks");
+
+form.addEventListener("submit", addTask);
+
+// Add Task
+function addTask(e) {
+    e.preventDefault();
+    let value = document.querySelector("#task").value;
+    
+    if(value) {
+        let li = document.createElement("li");
+        li.className = "task";
+        li
+    }
+    
+    console.log(value);
+}
