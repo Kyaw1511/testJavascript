c8ba5e172a5bbd7182ebc241e23e14ea64b3ca44
Kyaw1511 <kyawminthein151193@gmail.com>
2024-07-30 02:47:11 +0630

    to do list v1.0

--- toDoList.js
+++ toDoList.js
@@ -12,8 +12,30 @@ function addTask(e) {
     if(value) {
         let li = document.createElement("li");
         li.className = "task";
-        li
-    }
-    
+        li.innerHTML = `
+            <span class="done" > ${value} </span> 
+            <i class='bx bx-x remove'></i>
+        `;
+        tasks.appendChild(li);
+        document.querySelector("#task").value = "";
+    };
     console.log(value);
+};
+
+// remove Task
+tasks.addEventListener("click", removeTask);
+
+function removeTask(e) {
+    let rem = e.target;
+
+    if(rem.classList.contains("remove")) {
+        rem.parentElement.remove();
+        console.log(rem);
+    }
+    else if(rem.classList.contains("done")) {
+        rem.parentElement.style.backgroundColor = "#87CEEB";
+        rem.style.textDecoration = "line-through";
+        rem.style.color = "#444";
+    }
+    console.log(e.target);
 }
