89571a706e30ce4081d387d775b20aaf6f4dc868
Kyaw1511 <kyawminthein151193@gmail.com>
2024-08-11 18:50:49 +0630

    dark mode v1.5

--- darkmode.js
+++ darkmode.js
@@ -3,7 +3,9 @@ const navMenu = document.querySelector('.nav');
 
 hamburger.addEventListener('click', () => {
     navMenu.classList.toggle('hide');
-    navMenu.classList.add("test");
+    // navMenu.classList.add("test");
+    navMenu.classList.add("test1");
+    
     console.log("Hi")
-    console.log(navMenu.classList.value);
+    console.log(navMenu);
 });
