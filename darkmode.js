b975edc5af417f334f265c4a5c1c0b5ae9128484
Kyaw1511 <kyawminthein151193@gmail.com>
2024-08-12 20:46:53 +0630

    dark mode v1.6 js

--- darkmode.js
+++ darkmode.js
@@ -1,5 +1,7 @@
 const hamburger = document.querySelector('.hamburger-menu');
 const navMenu = document.querySelector('.nav');
+const toggle = document.querySelector("#toggle-btn");
+const icon = document.querySelector("#toggle-icon");
 
 hamburger.addEventListener('click', () => {
     navMenu.classList.toggle('hide');
@@ -9,3 +11,25 @@ hamburger.addEventListener('click', () => {
     console.log("Hi")
     console.log(navMenu);
 });
+
+toggle.addEventListener("click", darkModeToggle);
+let darkMode = false;
+
+function darkModeToggle() {
+    if(darkMode) {
+        darkModeOff();
+    }
+    else {
+        darkModeOn();
+    }
+};
+// call dark mode on 
+function darkModeOn() {
+    document.body.classList.add("dark");
+    darkMode = true;
+}
+// call dark mode off
+function darkModeOff() {
+    document.body.classList.remove("dark");
+    darkMode = false;
+}
