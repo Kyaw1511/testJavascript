58442720c3b7418903117701cef3a16879e69323
Kyaw1511 <kyawminthein151193@gmail.com>
2024-08-13 21:43:53 +0630

    dark mode v1.7 localStorage

--- darkmode.js
+++ darkmode.js
@@ -1,22 +1,35 @@
 const hamburger = document.querySelector('.hamburger-menu');
 const navMenu = document.querySelector('.nav');
 const toggle = document.querySelector("#toggle-btn");
-const icon = document.querySelector("#toggle-icon");
+const icon = document.querySelector("#toggle_icon");
 
 hamburger.addEventListener('click', () => {
     navMenu.classList.toggle('hide');
     // navMenu.classList.add("test");
-    navMenu.classList.add("test1");
+
+    let test = document.querySelector(".test");
+    if(test) {
+        navMenu.classList.add("test");
+    }
+    else {
+        navMenu.classList.remove("test");
+    }
     
     console.log("Hi")
     console.log(navMenu);
 });
 
 toggle.addEventListener("click", darkModeToggle);
-let darkMode = false;
+let darkMode = localStorage.getItem("darkMode");
+
+// darkMode check
+if(darkMode === "enabled") {
+    darkModeOn();
+}
 
 function darkModeToggle() {
-    if(darkMode) {
+    darkMode = localStorage.getItem("darkMode");
+    if(darkMode === "enabled") {
         darkModeOff();
     }
     else {
@@ -27,9 +40,13 @@ function darkModeToggle() {
 function darkModeOn() {
     document.body.classList.add("dark");
     darkMode = true;
+    localStorage.setItem("darkMode", "enabled");
+    icon.className = "bx bxs-moon";
 }
 // call dark mode off
 function darkModeOff() {
     document.body.classList.remove("dark");
     darkMode = false;
+    localStorage.setItem("darkMode", null);
+    icon.className = "bx bxs-sun";
 }
