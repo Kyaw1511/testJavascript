d06870d73fac41b9482b1eb3b6259acaa634650f
Kyaw1511 <kyawminthein151193@gmail.com>
2024-08-16 20:22:10 +0630

    ObjectOriented1.2

--- object3.js
+++ object3.js
@@ -13,4 +13,34 @@ let people1 = {
 }
 people1.biology();
 document.getElementById("test").innerHTML = people1.name;
-// document.getElementById("test1").innerHTML = people1.biology()
+
+// class name
+class human {
+    age = 19;
+    hobbies = ["Gym", "Coding"];
+    bios() {
+        console.log("Hello");
+    }
+}
+let people2 = new human();
+let people3 = human;
+console.log(typeof(people2));
+console.log(typeof(human));
+console.log(typeof(people1));
+console.log(typeof(people3));
+
+document.querySelector(".test2").innerHTML = people2.age;
+
+class human1 {
+    static age = 22;
+    static hobbies = ["Gym", "Reading"];
+    static biology1() {
+        console.log("humanOne");
+    }
+}
+console.log(typeof(human1));
+
+// Public
+// private
+// Protected
+// Static
