ec30e7072d2a5d09f8cde449fefda151e73c6419
Kyaw1511 <kyawminthein151193@gmail.com>
2024-08-18 20:15:57 +0630

    movieProjectV2.0

--- movie.js
+++ movie.js
@@ -1,8 +1,32 @@
-// Menu toggle
+
 const menuToggle = document.querySelector("#menu-toggle");
+const SettingToggle = document.querySelector("#setting-toggle");
+const images = Array.from(document.querySelectorAll(".setting-box img"));
+const landingImg = document.querySelector(".landing-image");
 
-
+// Menu Toggle
 menuToggle.addEventListener("click", () => {
-    const navMenu = document.querySelector("#nav-menu");
+    let navMenu = document.querySelector("#nav-menu");
     navMenu.classList.toggle("nav-active");
 })
+
+// Setting Toggle
+SettingToggle.addEventListener("click", () => {
+    let settingBox = document.querySelector(".setting-box");
+    settingBox.classList.toggle("setting-box-active");
+})
+
+// Setting box image
+images.map((image) => {
+    image.addEventListener("click", () => {
+        images.forEach((img) => {
+            img.style.opacity = "1";
+        })
+        landingImg.src = image.src;
+        image.style.opacity = "0.5";
+
+        console.log(image);
+        console.log(image.src);
+    })
+})
+console.log(images);
