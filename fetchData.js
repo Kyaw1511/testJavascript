3e05b6dc34c916c79104ba0751bf4139ecee793b
Kyaw1511 <kyawminthein151193@gmail.com>
2024-09-10 20:20:47 +0630

    using fetch apiV1.1

--- fetchData.js
+++ fetchData.js
@@ -3,22 +3,43 @@ let reqresFetch = document.querySelector("#reqresFetch");
 let text = "";
 
 
-fetch("https://reqres.in/api/users?page=2")
-.then(answer => answer.json())
-.then(res => {
-    for(let i = 0; i < res.length; i++) {
-        text += `<td> ${res.data[i]} </td> <br>`;
-        console.log(data[i]);
-    }
-    console.log(res);
-    console.log(res.data[1].first_name);
+fetch("https://reqres.in/api/users")
+// .then(answer => answer.json())
+.then(response => response.json())
+.then(data => {
+    displayUserData(data.data);
+    // for(let i = 0; i < res.length; i++) {
+    //     text +=i + " " + res.data[i].firstname + "<br>";
+    //     // text += `<td> ${data.data[i]} </td> <br>`;
+    //     console.log(data[i]);
+    // }
+    
+    console.log(data);
+    console.log(typeof(data));
+    console.log(data.data[1].first_name);
 
     // reqresFetch.innerHTML = data.data[1].first_name;
     // reqresFetch.innerHTML = data[1].first_name;
     // reqresFetch.innerHTML = res.data[1].email;
     // console.log(text.data[1].first_name)
-    reqresFetch.innerHTML = text.value;
+    // reqresFetch.innerHTML = text.first_name;
 });
+function displayUserData(users) {
+    const userDataEle = document.getElementById("userData");
+    userDataEle.innerHTML = "";
+
+    users.forEach(user => {
+        const userDiv = document.createElement("div");
+        userDiv.classList.add("user");
+
+        const name = document.createElement("p");
+        name.textContent = `Name : ${user.first_name}`;
+
+        userDiv.appendChild(name);
+        userDataEle.appendChild(userDiv);
+        console.log(user)
+    })
+}
 
 async function fetchDataRequ() {
     let data = await fetch("https://reqres.in/api/users");
@@ -27,6 +48,8 @@ async function fetchDataRequ() {
     console.log(firstname.data[2].first_name);
 }
 fetchDataRequ();
+
+
 
 // for(let i = 0; i < arr.length; i++) {
 //     for(let x = 0; x < arr[i].length; x++) {
