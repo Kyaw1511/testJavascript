00d3f5e7ff37fb9ad68498cb345e6d5468feb1c9
Kyaw1511 <kyawminthein151193@gmail.com>
2024-09-11 07:34:54 +0630

    using fetch api V1.2

--- fetchData.js
+++ fetchData.js
@@ -6,22 +6,22 @@ let text = "";
 fetch("https://reqres.in/api/users")
 // .then(answer => answer.json())
 .then(response => response.json())
-.then(data => {
-    displayUserData(data.data);
+.then(res => {
+    displayUserData(res.data);
     // for(let i = 0; i < res.length; i++) {
     //     text +=i + " " + res.data[i].firstname + "<br>";
     //     // text += `<td> ${data.data[i]} </td> <br>`;
     //     console.log(data[i]);
     // }
     
-    console.log(data);
-    console.log(typeof(data));
-    console.log(data.data[1].first_name);
+    console.log(res);
+    console.log(typeof(res));
+    console.log(res.data[1].first_name);
 
     // reqresFetch.innerHTML = data.data[1].first_name;
     // reqresFetch.innerHTML = data[1].first_name;
-    // reqresFetch.innerHTML = res.data[1].email;
-    // console.log(text.data[1].first_name)
+    // reqresFetch.innerHTML = res.[1].email;
+    // console.log(text.[1].first_name)
     // reqresFetch.innerHTML = text.first_name;
 });
 function displayUserData(users) {
@@ -32,12 +32,27 @@ function displayUserData(users) {
         const userDiv = document.createElement("div");
         userDiv.classList.add("user");
 
+        // fetch first_name from object
         const name = document.createElement("p");
         name.textContent = `Name : ${user.first_name}`;
 
+        // fetch email from object
+        const email = document.createElement("email");
+        email.textContent = `Email : ${user.email}`;
+
+        // fetch avatar from object
+        const avatar = document.createElement("img");
+        avatar.src = user.src;
+        avatar.alt = `Avatar of ${user.first_name} ${user.last_name}`;
+
+        // appendChild 
+        userDiv.appendChild(avatar);
         userDiv.appendChild(name);
+        userDiv.appendChild(email);
+        
         userDataEle.appendChild(userDiv);
-        console.log(user)
+        console.log(user);
+        console.log(user.avatar);
     })
 }
 
